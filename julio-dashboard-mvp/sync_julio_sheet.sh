e94517b85d0b1fb29dd53223908297026baf1f6b
Andrés Villegas <andresvillegas@Mac-mini-de-Andres.local>
2026-04-15 10:15:45 -0400

    Preserve Sphere layer after dashboard sync

--- julio-dashboard-mvp/sync_julio_sheet.sh
+++ julio-dashboard-mvp/sync_julio_sheet.sh
@@ -8,3 +8,4 @@ export SETTLEMENTS_CSV_URL='https://docs.google.com/spreadsheets/d/19geWVUxQvBnv
 export OPERATIONS_CSV_URL='https://docs.google.com/spreadsheets/d/19geWVUxQvBnvwwgycW3aF3n68YV8usljQrQx63kLUAg/export?format=csv&gid=2059476643'
 
 node sync_from_google_sheets.js
+node merge_sphere_into_current.js
